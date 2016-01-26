92a9e2e90d11972607f1f9c22005df24edf611ec
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-01-26 16:17:04 +0000

    Fix compilation warning

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS += -Ofast
+LOCAL_CFLAGS += -O3 -Wno-error=implicit-function-declaration
 LOCAL_MODULE := libopendelta
 LOCAL_SRC_FILES += xdelta3-3.0.7/xdelta3.c zipadjust.c delta.c delta_jni.c
 LOCAL_LDLIBS := -lz
